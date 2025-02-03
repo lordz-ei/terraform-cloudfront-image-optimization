7dd475bbc786f57b8982182bb91d85c77c71007f
Traian Ciobanu <traian.ciobanu@aerlingus.com>
2025-02-03 13:34:08 +0200

    fix: LZ-4, fix cloudfront_url_rewrite file name

--- cloudfront_function.tf
+++ cloudfront_function.tf
@@ -2,5 +2,5 @@ resource "aws_cloudfront_function" "cloudfront_url_rewrite" {
   name    = "cloudfront-url-rewrite"
   runtime = "cloudfront-js-1.0"
   publish = true
-  code    = file("./src/url-rewrite/url-rewrite.js")
+  code    = file("./src/url-rewrite/index.js")
 }
